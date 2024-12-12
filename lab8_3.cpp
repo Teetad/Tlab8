9c672b7333be25b12d2887d008615592b72c8115
Teetad <teetadraseesit@gmail.com>
2024-12-12 22:47:48 +0700

    lab8_3 done

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,42 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    string your_character;
+    cout << "Enter your age: ";
+    int age;
+    cin >> age;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        int height;
+        cin >> height;
+        if (height < 100){
+            your_character = "Chopper";
+        }else{
+            if (height < 180){
+                your_character = "Usopp";
+            }else{cout << "Enter your bounty: ";
+                int bounty;
+                cin >> bounty;
+                if (bounty > 1100000000){
+                 your_character = "Zoro";
+                    }else{
+                        your_character = "Sanji";
+                }
+            }
+        }
+    }else if (age <= 60){
+        cout << "Enter your bounty: ";
+        int bounty;
+        cin >> bounty;
+        if (bounty > 500000000){
+            your_character = "Jinbe";
+        }else{
+            your_character = "Franky";
+        }
+    }else{
+        your_character = "Brook";
+    }
+    cout << "Your character = " << your_character; 
+
+}
